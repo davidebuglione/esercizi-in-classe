b5625b28e825edaf628d61e1c1b7dd79786340e7
Davide Buglione <davidebuglione@MBP-di-Davide.homenet.telecomitalia.it>
2021-04-08 02:18:56 +0200

    complete version

--- terminal_game.py
+++ terminal_game.py
@@ -40,7 +40,7 @@ class Entity:
 
     def collide(self, entity):
         entity.assign_coords(entity, randint(0,w_field-1), randint(0,h_field-1))
-
+        snake.score += 1
     def assign_coords(self, entity, x, y):
         entity.x = x
         entity.y = y
@@ -49,6 +49,7 @@ class Entity:
 class Snake(Entity):
     def __init__(self, x, y):
         super().__init__(x, y)
+        self.score = 0
 
 #creazione classe Field
 class Field:
@@ -90,11 +91,11 @@ def clear_screen():
     else:
         os.system("clear")
 
-#auto_move = "right"
 class Motion_thread(Thread):
     def __init__(self):
         Thread.__init__(self)
         self.auto_move = "right"
+
     def run(self):
         while True:
             if keyboard.is_pressed("w"):
@@ -115,7 +116,10 @@ class Motion_thread(Thread):
                     continue
             
             if keyboard.is_pressed("0"):
-                quit()        
+                quit()
+
+            if end == "si":
+                quit()     
 
 #intro
 clear_screen()
@@ -134,13 +138,14 @@ field.entities.append(snake)
 field.entities.append(point)
 
 motion_thread = Motion_thread()
-
 clear_screen()
 
 motion_thread.start()
 #ciclo di gioco
+end = "no"
 while True:
     field.draw_field()
+    print("score:",snake.score)
 
     if motion_thread.auto_move == "right":
         snake.move("d")
@@ -150,29 +155,15 @@ while True:
         snake.move("w")
     elif motion_thread.auto_move == "down":
         snake.move("s")
-    '''
-    if keyboard.is_pressed("w"):
-        auto_move = "up"
-        while keyboard.is_pressed("w"):
-            continue
-    elif keyboard.is_pressed("s"):
-        auto_move = "down"
-        while keyboard.is_pressed("s"):
-            continue
-    elif keyboard.is_pressed("d"):
-        auto_move = "right"
-        while keyboard.is_pressed("d"):
-            continue
-    elif keyboard.is_pressed("a"):
-        auto_move = "left"
-        while keyboard.is_pressed("a"):
-            continue
-    
-    if keyboard.is_pressed("0"):
-        quit()
-    '''
+
     if snake.y == -1 or snake.y == field.h or snake.x == field.w or snake.x == -1:
+        end = "si"
         print("sei morto")
         quit()
-    time.sleep(0.5)
+
+    if snake.score <= 3:
+        time.sleep(1)
+    else:
+        time.sleep(0.5)
+
     clear_screen()
